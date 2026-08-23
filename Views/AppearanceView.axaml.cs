c13405f83a0481c48a5291b98c5901fb23572f2c
Dulmina03 <dulminajayasiri@gmail.com>
2026-08-23 23:57:39 +0530

    feat: implement Appearance and Notifications settings views with associated UI logic

---
+++ Views/AppearanceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DataSense.Views;
+
+public partial class AppearanceView : UserControl
+{
+    public AppearanceView()
+    {
+        InitializeComponent();
+    }
+}
